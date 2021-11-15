36f506daa2338fdc21f9067b92d25952601ec8e3
Mathias <mathias.skallerud@gmail.com>
2021-11-15 14:59:18 +0100

    Signout button

--- src/components/LoggedInHeader.tsx
+++ src/components/LoggedInHeader.tsx
@@ -1,8 +1,12 @@
+import { useCookies } from "react-cookie";
+
 type HeaderProps = {
   setPage: (arg0: number) => void,
 };
 
 export const LoggedInHeader = (props: HeaderProps) => {
+  const [cookie, setCookie, removeCookie] = useCookies(["token", "username"])
+
   return (
     <div className="App">
       <h1>Welcome to FeedApp</h1>
@@ -11,8 +15,10 @@ export const LoggedInHeader = (props: HeaderProps) => {
       <button onClick={() => props.setPage(4)}>Add new poll</button>
       <button onClick={() => props.setPage(5)}>My page</button>
       <button onClick={() => props.setPage(6)}>Search</button>
-
-
+      <button onClick={() => {
+        removeCookie("username");
+        removeCookie("token")
+      }}>Sign out</button>
       </div>
   );
 };
